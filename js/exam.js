c98ffcfce0bb3808836e943865654a166d4ca291
byeonggon <qudrhs27@gmail.com>
2026-08-05 18:04:47 +0900

    8월 5일 js 기본 2

--- js/exam.js
+++ js/exam.js
@@ -3,15 +3,16 @@ function sum(num1, num2) {
   return num1 + num2;
 }
 console.log(sum(10, 15));
+
 // 2. 함수 안에서 1~10까지 더한 결과를 출력하는 함수 작성
 function hap() {
-  result = 0;
+  let result = 0;
   for (let index = 1; index <= 10; index++) {
     result += index;
   }
-  return result;
+  console.log(result);
 }
-console.log(hap());
+hap();
 
 // 3. multiple(num) 정의 - num이 3의 배수라면 "박수" 출력 / "통과"
 function multiple(num) {
@@ -22,18 +23,23 @@ function multiple(num) {
   }
 }
 multiple(12);
+multiple(5);
 
 // 4. multiple2(num) - 3의 배수인 경우 "박수" / 9의 배수 "박수*2" / "통과"
 function multiple2(num) {
   if (num % 3 == 0) {
-    console.log("박수");
-  } else if (num % 9 == 0) {
-    console.log("박수*2");
+    if (num % 9 == 0) {
+      console.log("박수*2");
+    } else {
+      console.log("박수");
+    }
   } else {
     console.log("통과");
   }
 }
-multiple2(36);
+multiple2(3);
+multiple2(15);
+multiple2(9);
 
 // 5. pass(outline, law) - 두 과목 합해서 120점 이상이면 합격
 // 단, 한 과목이라도 40점 미만이면 과락으로 불합격
